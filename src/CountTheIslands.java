c1878814ef5f2d6161222251cec82a06cfcf8776
Michael Lynn <hailviral101@yahoo.com>
2019-11-14 20:02:06 -0700

    number of islands
    sanity check

---
+++ src/CountTheIslands.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class CountTheIslands {
+
+    public static void main(String test[]){
+
+
+        Integer myMatrix[][] = {
+                { 1, 1, 0, 0, 0 },
+                { 0, 1, 0, 0, 1 },
+                { 1, 0, 0, 1, 1 },
+                { 0, 0, 0, 0, 0 },
+                { 1, 0, 1, 0, 1 }
+        };
+
+    }
+
+
+}
--- src/DiagonalDifference.java
+++ src/DiagonalDifference.java
@@ -39,9 +39,11 @@ public class DiagonalDifference {
 
     public static void main(String test[]){
 
-        Integer myArray[][] = {{11, 2, 4},
+        Integer myArray[][] = {
+                        {11, 2, 4},
                          {4, 5, 6},
-                         {10, 8, -12}};
+                         {10, 8, -12}
+        };
 
         List<List<Integer>> arrayList2D = new ArrayList(myArray.length);
 
